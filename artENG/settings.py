eac6c89247348f6c010497133c9627b61ceccf5a
Tomislav Celić <celer.tomislav@gmail.com>
2024-02-22 23:04:38 +0100

    Make webapp production ready

--- artENG/settings.py
+++ artENG/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static
+STATIC_URL = '/static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y4+&0k#w0uo-umw3bdys*2j$p*=e&!_avggeryloz01pv^3jl-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = ['192.168.100.39', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
